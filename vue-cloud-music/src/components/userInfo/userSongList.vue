daa474df4df1486ed26cbae7bcf5d8146ffddb55
wangzhaojie <wangzhaojie@antiy.cn>
2018-06-29 20:03:09 +0800

    Dev: 完善用户信息
    Signed-off-by: wangzhaojie <wangzhaojie@antiy.cn>

--- vue-cloud-music/src/components/userInfo/userSongList.vue
+++ vue-cloud-music/src/components/userInfo/userSongList.vue
@@ -10,15 +10,37 @@
         </div>
       </div>
     </div>
-    <span>{{songList.userId}}</span>
+    <div class="list-container">
+      <div class="list-item" v-for="(item, index) in list">
+        <div class="item-img">
+          <img :src="item.coverImgUrl">
+        </div>
+        <div class="r">
+          <div>
+            <div class="title">
+              <span>{{item.name.length < 15? item.name: `${item.name.substring(0, 15)}...`}}</span>
+            </div>
+            <div class="info">
+              <span>{{item.trackCount}}首，播放{{initCollectionNumber(item.playCount)}}次</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+  import {getUserPlaylist} from '@/common/js/axiosType/getAxiosType.js'
   export default {
     name: 'userSongList',
     data () {
       return {
-        num: this.songList.playlistBeSubscribedCount,
+        list: []
+      }
+    },
+    computed: {
+      userId () {
+        return this.songList.userId
       }
     },
     props: {
@@ -40,11 +62,27 @@
         if (num > 10000000) {
           return `${num.toString().substring(0, 4)}.${num.toString().substring(4, 5)}万`
         }
+
+        return num
+      },
+      getUserSongList (result) {
+        console.log(result)
+        this.list = result.data.playlist
       }
     },
     mounted () {
-//      this.initCollectionNumber();
+      console.log(this.userId);
+      getUserPlaylist(this.userId).then(result => {
+        this.getUserSongList(result)
+      })
     },
+    watch: {
+      userId (newValue, oldValue) {
+        getUserPlaylist(newValue).then(result => {
+          this.getUserSongList(result)
+        })
+      }
+    }
   }
 </script>
 <style lang="less" scoped>
@@ -71,6 +109,56 @@
         .collection {
           color: @themeGray;
         }
+      }
+    }
+
+    .list-container {
+      width: 98%;
+      margin-left: 2%;
+      background-color: #fff;
+
+      .list-item {
+        display: flex;
+        align-items: center;
+        position: relative;
+
+        & > div:nth-child(2) {
+          padding-left: 10px;
+        }
+
+        .item-img {
+          width: 15%;
+          padding: 5px 0;
+
+          & > img {
+            display: block;
+            border-radius: 5px;
+            width: 100%;
+          }
+        }
+
+        .r {
+          position: absolute;
+          display: flex;
+          align-items: center;
+          top: 0;
+          bottom: 0;
+          right: 0;
+          left: 16%;
+          border-bottom: 1px #EBECF7 solid;
+
+          .title {
+            color: @themeBlack;
+          }
+          .info {
+            text-align: left;
+            margin-top: 10px;
+            color: @themeGray;
+            font-size: .7rem;
+          }
+        }
+
+
       }
     }
   }
