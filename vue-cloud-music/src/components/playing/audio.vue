7327c9b59595641a3a1ef8b071ded43c4d2831f3
wangzhaojie <wangzhaojie@antiy.cn>
2018-07-05 11:22:33 +0800

    Fix: current time fix current minute show correctly while playing
    Signed-off-by: wangzhaojie <wangzhaojie@antiy.cn>

--- vue-cloud-music/src/components/playing/audio.vue
+++ vue-cloud-music/src/components/playing/audio.vue
@@ -33,7 +33,8 @@
     },
     methods: {
       timeUpdate (e) {
-        let min = (e.path[0].currentTime / 60).toFixed(0);
+//        console.log(e);
+        let min = parseInt((e.path[0].currentTime / 60), 10);
         let sec = (e.path[0].currentTime % 60).toFixed(0);
         if (sec < 10) sec = `0${sec}`;
         this.currentTime = `${min}:${sec}`;
